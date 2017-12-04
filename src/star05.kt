a5e97e5e27ed60ad979d64bcfd30b9e4c1c8888c
Trevor Lalish-Menagh <trev@trevreport.org>
2017-12-04 01:26:41 -0800

    Still on Star 5.
    Going to bed.

--- src/star05.kt
+++ src/star05.kt
@@ -3,15 +3,38 @@ import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
-    val start = 23 //Integer.parseInt(args[0])
+    val start = 19 //Integer.parseInt(args[0])
     val sqrtStart = roundUp(sqrt(start.toDouble()))
     val size = if (sqrtStart % 2 == 0) sqrtStart + 1 else sqrtStart
     val center = roundUp(size / 2.0) // q1, q2 == center, center
-    val lastNumber = (pow((size).toDouble(), 2.0)).toInt()
+    val last = (pow((size).toDouble(), 2.0)).toInt()
+    val bottomLeft = last - size + 1
+    val topLeft = bottomLeft - size + 1
+    val topRight = topLeft - size + 1
+    val first = topRight - size + 2
+    val position = size % (last - start) // TODO: nope. Not right yet. 1:30 am going to bed.
 
     // TODO: find p1, p2 from start.
+    /*
+    1,1 ... 1,size
+    .
+    .
+    .
+    size,1 ... size,size
+     */
+    when {
+        start > bottomLeft -> println("[$size, $position]")
+        start > topLeft -> println("[$position, 1]")
+        start > topRight -> println("[1, $position]")
+        else -> println("[$position, $size]")
+    }
 
-    println(size.toString() + ": [" + center.toString() + ", " + center.toString() + "], last: " + lastNumber.toString() + ", diff: " + (lastNumber - start).toString())
+    println(size.toString() + ": [" + center.toString() + ", " + center.toString() + "], br: "
+            + last.toString()
+            + ", bl: " + bottomLeft.toString()
+            + ", tl: " + topLeft.toString()
+            + ", tr: " + topRight.toString()
+            + ", first: " + first.toString())
 }
 
 fun roundUp(num : Double) : Int {
