5f4389b8863578e8ec9a88f46dc70bf8e57a119e
Trevor Lalish-Menagh <trev@trevreport.org>
2017-12-04 20:13:59 -0800

    Got Star 5!!!

--- src/star05.kt
+++ src/star05.kt
@@ -1,9 +1,13 @@
+import java.lang.Math.abs
 import java.lang.Math.pow
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
 fun main(args: Array<String>) {
-    val start = 17 //Integer.parseInt(args[0])
+    println(findManhattanDistance(368078))
+}
+
+fun findManhattanDistance(start: Int) : Int {
     val sqrtStart = roundUp(sqrt(start.toDouble()))
     val size = if (sqrtStart % 2 == 0) sqrtStart + 1 else sqrtStart
     val center = roundUp(size / 2.0) // q1, q2 == center, center
@@ -11,29 +15,22 @@ fun main(args: Array<String>) {
     val bottomLeft = last - size + 1
     val topLeft = bottomLeft - size + 1
     val topRight = topLeft - size + 1
-    val first = topRight - size + 2
 
-    // TODO: find p1, p2 from start.
     /*
-    1,1 ... 1,size
+    1,1 ... size,1
     .
     .
     .
-    size,1 ... size,size
+    1,size ... size,size
      */
-    when {
-        start > bottomLeft -> println("b[$size, ${if (last - start == 0) size else last - start}]")
-        start > topLeft -> println("l[${if (bottomLeft - start == 0) size else bottomLeft - start + 1}, 1]")
-        start > topRight -> println("t[1, ${topLeft - start + 1}]")
-        else -> println("r[${topRight - start + 1}, $size]")
+    val location =  when {
+        start >= bottomLeft -> Pair(size - (last - start), size)
+        start >= topLeft -> Pair(1, size - (bottomLeft - start))
+        start >= topRight -> Pair(topLeft - start + 1, 1)
+        else -> Pair(size, topRight - start + 1)
     }
 
-    println(size.toString() + ": [" + center.toString() + ", " + center.toString() + "], br: "
-            + last.toString()
-            + ", bl: " + bottomLeft.toString()
-            + ", tl: " + topLeft.toString()
-            + ", tr: " + topRight.toString()
-            + ", first: " + first.toString())
+    return abs(location.first - center) + abs(location.second - center)
 }
 
 fun roundUp(num : Double) : Int {
